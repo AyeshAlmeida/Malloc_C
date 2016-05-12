42c467803014d6adb42a3ddee5f2f118d1220a78
ayesh123 <ayesh9206@gmail.com>
2016-05-12 18:08:06 +0530

    Header file of Malloc

---
+++ malloc_new.h
@@ -0,0 +1,12 @@
+/************************** Malloc in C - without structures ***************************
+*************************** Dynamic Memory allocation in C   ***************************
+*************************** Author : Ayesh Almeida	     ***************************
+*************************** Date   : 12-05-2016              ***************************
+*************************** Moratuwa, Sri Lanka              ***************************/
+
+
+/*Function prototypes*/
+
+//Malloc Function
+void *MyMalloc(size_t s);
+void MyFree(void *p);
